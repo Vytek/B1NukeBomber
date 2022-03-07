3dad347349aad4c992e408f05e6520c93af4c80b
Enrico Speranza <enrico.speranza@gmail.com>
2022-03-07 21:08:41 +0100

    Update to use Defence position

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/fatih/color"
 	"github.com/gocarina/gocsv"
 	"github.com/mmcloughlin/spherand"
-	"github.com/rbsns/golang-geo"
+	geo "github.com/rbsns/golang-geo"
 	"github.com/scylladb/termtables"
 )
 
@@ -189,19 +189,21 @@ func Navigation(data string) string {
 	p_b1 := geo.NewPoint(b1.lat, b1.long)
 	for e := ListTargets.Front(); e != nil; e = e.Next() {
 		itemTarget := Target(e.Value.(Target))
-		p_target := geo.NewPoint(itemTarget.lat, itemTarget.long)
-		dist := p_b1.GreatCircleDistance(p_target)
-		bearing := p_b1.BearingTo(p_target)
-		//Fix: https://github.com/rbsns/golang-geo/commit/8004ce49479db5787a6cab51b37b38e9ce052ca5
-		if bearing < 0. {
-			bearing = 360. + bearing
-		}
-		if len(data) == 0 {
-			//s = s + fmt.Sprintf("Name: "+itemTarget.name+" Abbr.: "+itemTarget.abbreviation+" Dist.: %.2f Km\n", dist)
-			table.AddRow(itemTarget.name, itemTarget.abbreviation, fmt.Sprintf("%.2f Km", dist), fmt.Sprintf("%.2f", bearing))
-		} else {
-			if itemTarget.name == data || itemTarget.abbreviation == data {
+		if itemTarget.targetype == "C" {
+			p_target := geo.NewPoint(itemTarget.lat, itemTarget.long)
+			dist := p_b1.GreatCircleDistance(p_target)
+			bearing := p_b1.BearingTo(p_target)
+			//Fix: https://github.com/rbsns/golang-geo/commit/8004ce49479db5787a6cab51b37b38e9ce052ca5
+			if bearing < 0. {
+				bearing = 360. + bearing
+			}
+			if len(data) == 0 {
+				//s = s + fmt.Sprintf("Name: "+itemTarget.name+" Abbr.: "+itemTarget.abbreviation+" Dist.: %.2f Km\n", dist)
 				table.AddRow(itemTarget.name, itemTarget.abbreviation, fmt.Sprintf("%.2f Km", dist), fmt.Sprintf("%.2f", bearing))
+			} else {
+				if itemTarget.name == data || itemTarget.abbreviation == data {
+					table.AddRow(itemTarget.name, itemTarget.abbreviation, fmt.Sprintf("%.2f Km", dist), fmt.Sprintf("%.2f", bearing))
+				}
 			}
 		}
 	}
